e86e51f61db96bcdb448594d0e0e8b17494bb5c7
gowthaaman <gokrishna98@gmail.com>
2025-11-10 16:13:57 +0530

    Describe your new changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,10 +7,37 @@ export default {
   theme: {
     container: {
       center: true,
-      padding: "2rem",
+      padding: {
+        DEFAULT: "1rem",
+        sm: "1.5rem",
+        md: "2rem",
+        lg: "2rem",
+        xl: "2.5rem",
+        "2xl": "3rem",
+      },
       screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
         "2xl": "1400px",
       },
+    },
+    screens: {
+      'xs': '375px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+      '3xl': '1920px',
+      // Device-specific breakpoints
+      'iphone': '375px',
+      'iphone-plus': '414px',
+      'ipad': '768px',
+      'ipad-pro': '1024px',
+      'desktop': '1280px',
+      'imac': '1920px',
     },
     extend: {
       colors: {
